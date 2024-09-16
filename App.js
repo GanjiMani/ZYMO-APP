83f1070ed4342bfe5d0b6a4bb684eb7869a00591
GanjiMani <137642832+GanjiMani@users.noreply.github.com>
2024-09-16 11:44:40 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+// src/App.js
+   import React from 'react';
+   import FeaturePage from './components/FeaturePage';
+
+   function App() {
+     return (
+       <div className="App">
+         <FeaturePage />
+       </div>
+     );
+   }
+
+   export default App;
